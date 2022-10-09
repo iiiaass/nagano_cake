74fb0b71e92ae65b8ca005dcc7af0659c69cbfdb
EC2 Default User <ec2-user@ip-172-31-7-78.us-west-1.compute.internal>
2022-10-09 13:00:34 +0000

    [Update]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items=Item.page(params[:page])
   end
 
   def show
@@ -7,6 +8,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item=Item.find(params[:id])
   end
 
   def new
@@ -22,9 +24,16 @@ class Admin::ItemsController < ApplicationController
     # end
   end
 
+  def update
+    @item=Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+  end
+
+
   private
   def item_params
-    params.require(:item).permit(:genre_id,:name,:introduction,:price,:is_active,:get_image)
+    params.require(:item).permit(:genre_id,:name,:introduction,:price,:is_active,:image)
   end
 
 end
