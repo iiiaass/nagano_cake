2532c735c1f8cfe8ad657859967cf61445d60563
EC2 Default User <ec2-user@ip-172-31-7-78.us-west-1.compute.internal>
2022-10-10 13:56:07 +0000

    [Update]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,15 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
+  def create
+    @cart_item=CartItem.new(cart_item_params)
+    @cart_item.customer_id=current_customer.id
+    @cart_item.save
+    redirect_to '/'
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:amount,:item_id)
+  end
 end
