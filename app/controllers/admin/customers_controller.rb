ad87f68b801e23f11d94cf816435bde461d1f8b3
EC2 Default User <ec2-user@ip-172-31-7-78.us-west-1.compute.internal>
2022-10-18 09:59:05 +0000

    [Update]nagano_cake

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,29 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers=Customer.page(params[:page])
   end
 
   def show
+    @customer=Customer.find(params[:id])
+    if @customer.is_deleted==true
+      @status = "退会"
+    else
+      @status = "有効"
+    end
   end
 
   def edit
+    @customer=Customer.find(params[:id])
+  end
+
+  def update
+    @customer=Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email,:is_deleted)
   end
 end
